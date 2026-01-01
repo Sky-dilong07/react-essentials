73fb5af8770c7397ba0ac9eb44037ab384478253
Saurabh Mishra <saurabhmishra12mgs@gmail.com>
2026-01-01 21:18:04 +0530

    React

---
+++ hello-world/src/App.jsx
@@ -0,0 +1,16 @@
+import { Welcome }  from "./Welcome";
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>React Compnonents </h1>
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
